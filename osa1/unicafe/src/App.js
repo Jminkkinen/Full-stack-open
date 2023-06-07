cdb747e2fd27ca74ba4e8f7e8a12ce1624890b41
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-06-07 15:59:36 +0300

    Unicafe incomplete

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const Statistics = (props) =>  {
+  const good = props.stats[0]
+  const neutral = props.stats[1]
+  const bad = props.stats[2]
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+  if (all !== 0) {
+    return(
+      <div>
+        <h1>
+          statistics
+        </h1>
+        <p>
+          good {good}
+        </p>
+        <p>
+          neutral {neutral}
+        </p>
+        <p>
+          bad {bad}
+        </p>
+        <p>
+          all {all}
+        </p>
+        <p>
+          average {average}
+        </p>
+        <p>
+          positive {positive} %
+        </p>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>
+        give feedback
+      </h1>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <Statistics stats={[good, neutral, bad]} />
+    </div>
+  )
+}
+
+export default App
