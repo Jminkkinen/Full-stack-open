66c5c867df6e949b40241fa3435b35d36ba89209
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-06-13 12:28:17 +0300

    osa1 anekdootit

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const Mostvotes = ({points, anecdotes}) => {
+  const big = Math.max(...points)
+  const anecdote = points.indexOf(big)
+  return (
+    <div>
+      <div>
+        {anecdotes[anecdote]}
+      </div>
+      <div>
+        has {big} votes
+      </div>
+    </div>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,26 +26,35 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-  const initialpoints = {
-    0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0
-  }
-
-  // const copy = { ...points }
 
   const [selected, setSelected] = useState(0)
+  console.log("selektoitu:", selected)
 
-  const [points, setPoints] = useState(initialpoints)
+  const [points, setPoints] = useState(new Array(8).fill(0))
+  console.log("points:", points)
+
+  const handleClick = () => {
+    console.log('click')
+    setPoints(points => {
+      const nextPoints = [...points]
+      nextPoints[selected] += 1
+      return nextPoints
+    })
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <div>{anecdotes[selected]}</div>
       <div>has {points[selected]} votes</div>
-      <button onClick={() => setPoints(...points, points[selected] += 1)}>
+      <button onClick={handleClick}>
         vote
       </button>
       <button onClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length)))}>
         next anecdote
       </button>
+      <h1>Anecdote with most votes</h1>
+      <Mostvotes points={points} anecdotes={anecdotes}/>
     </div>
 
   )
