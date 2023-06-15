55b4a316d60098aeee0ddb091d4c705d0a52b6d3
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-06-15 15:58:36 +0300

    osa2 puhelinluettelo temporary state

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return(
@@ -41,22 +42,31 @@ const Person = ({person}) => {
 
 const Persons = ({personsToShow}) => {
   return(
-    personsToShow.map((person) => <Person key={person.name} person={person} />
+    personsToShow.map((person) => <Person key={person.id} person={person} />
     )
   )
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
 
   const [newNumber, setNewNumber] = useState('')
 
   const [filterPerson, setFilterPerson] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -71,6 +81,11 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     else {
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        console.log(response)
+      })
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
