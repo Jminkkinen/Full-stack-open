10408f195ecfe641840be08cb66d7e0b97a37e99
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-06-15 14:36:59 +0300

    osa2 puhelinluettelo partial

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,46 +1,112 @@
 import { useState } from 'react'
 
+const Filter = (props) => {
+  return(
+      <div>
+      filter shown with: <input 
+        value={props.filterPerson}
+        onChange={props.handleFilterChange}
+      />
+      </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return(
+    <form onSubmit={props.addPerson}>
+      <div>
+        name: <input
+                value={props.newName}
+                onChange={props.handleNameChange}
+              />
+      </div>
+      <div>
+        number: <input
+                  value={props.newNumber}
+                  onChange={props.handleNumberChange}
+                />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Person = ({person}) => {
+  return(
+      <p>{person.name} {person.number}</p>
+  )
+}
+
+const Persons = ({personsToShow}) => {
+  return(
+    personsToShow.map((person) => <Person key={person.name} person={person} />
+    )
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-  const [newName, setNewName] = useState('asd')
+  const [newName, setNewName] = useState('')
 
-  const addName = (event) => {
+  const [newNumber, setNewNumber] = useState('')
+
+  const [filterPerson, setFilterPerson] = useState('')
+
+  const addPerson = (event) => {
     event.preventDefault()
-    const nameObject = {
+    const personObject = {
       name: newName,
+      number: newNumber,
     }
-  
-    setPersons(persons.concat(nameObject))
-    setNewName('')
+
+    const tuplikaatti = persons.map(person => person.name).includes(personObject.name)
+
+    if(tuplikaatti === true) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
   }
-  console.log(persons)
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilterPerson(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterPerson.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input
-                  value={newName}
-                  onChange={handleNameChange}
-                />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <p>
-        {persons.map((person) => person.name)}
-      </p>
-      <p></p> 
+      <Filter filterPerson={filterPerson} handleFilterChange={handleFilterChange} />
+      <h3>add a new</h3>
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 
