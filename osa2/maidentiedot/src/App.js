c8043321e0211a544beccb2667a1f2a400ff7d6e
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-06-29 15:58:51 +0300

    maidentiedot incomplete

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 const allcountries = 'https://studies.cs.helsinki.fi/restcountries/api/all'
 
+const api_key = process.env.API_KEY
+
 const Filter = (props) => {
   return(
       <div>
@@ -17,14 +19,34 @@ const Filter = (props) => {
 const Countryinfo = ({country}) => {
   const languages = Object.values(country.languages)
   const flag = country.flags.png
+  const lat = country.capitalInfo.latlng[0]
+  const lon = country.capitalInfo.latlng[1]
+  const coordurl = "http://api.openweathermap.org/data/2.5/"
+  let coordurlparams = new URL(`weather?lat=${lat}&lon=${lon}&appid=${apina_key}`, coordurl)
+  const [weatherdata, setWeatherdata] = useState('')
+  useEffect(() => {
+    axios
+      .get(coordurlparams)
+      .then(response => {
+        setWeatherdata(response.data)
+      })
+  }, [])
+  const temperature = weatherdata.main?.temp
+  const wind = weatherdata.wind?.speed
+  const icon = weatherdata?.weather[0]?.icon
+
   return(
     <div>
       <h2>{country.name.common}</h2>
       <div>{country.capital}</div>
       <div>Area {country.area}</div>
       <p><b>languages</b></p>
-      {languages.map(language => <div>&bull; {language}</div>)}
-      <p><img src={flag}/></p>
+      {languages.map(language => <div key={language}>&bull; {language}</div>)}
+      <p><img alt="" src={flag}/></p>
+      <h3>Weather in {country.capital}</h3>
+      <div>temperature {temperature - 273.15} Celcius</div>
+      <p><img alt="" src={`https://openweathermap.org/img/wn/${icon}@2x.png`}/></p>
+      <p>wind {wind} m/s</p>
     </div>
   )
 }
@@ -37,7 +59,6 @@ const Country = ({country, setFilterCountry}) => {
 }
 
 const Countries = ({countriesToShow, countries, setCountries, setFilterCountry}) => {
-  console.log(countriesToShow.length)
   if (countriesToShow.length < 2) {
     return(
       countriesToShow.map((country) => <Countryinfo key={country.name.common} country={country} />
